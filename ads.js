54bb47cada87dc1d25aa33bfb93242580e2b3bd6
Anny <quratulann_iqbal@hotmail.com>
2016-08-02 11:57:09 +0500

    new look of app changes logic plus design

--- ads.js
+++ ads.js
@@ -1,5 +1,7 @@
 var Books = (function () {
+    // constructor automatically assigns the argument values to class attributes whenever a new instance is generated
     function Books(titleOfBook, authorOfBook, subjectOfBook, priceOfBook) {
+        this.cName = "Books";
         this.title = titleOfBook;
         this.author = authorOfBook;
         this.subject = subjectOfBook;
@@ -12,6 +14,7 @@ var Books = (function () {
 }());
 var Cars = (function () {
     function Cars(nameOfCar, companyOfCar, modelOfCar, engineOfCar, colorOfCar, priceOfCar) {
+        this.cName = "Cars";
         this.name = nameOfCar;
         this.company = companyOfCar;
         this.model = modelOfCar;
@@ -26,6 +29,7 @@ var Cars = (function () {
 }());
 var Mobiles = (function () {
     function Mobiles(modelOfMobile, companyOfMobile, colorOfMobile, screenSizeOfMobile, priceOfMobile) {
+        this.cName = "Mobiles";
         this.model = modelOfMobile;
         this.company = companyOfMobile;
         this.color = colorOfMobile;
@@ -37,78 +41,120 @@ var Mobiles = (function () {
     };
     return Mobiles;
 }());
-// var book1 = new Books("HTML", "Ducket", "HTML", 1200);
+// var book1 = new Books("HTML", "Ducket", "HTML", 1200); // creating new instance (i.e.object) of a class
 var ad = [
-    { book1: new Books("HTML", "Ducket", "HTML", 200) },
-    { book2: new Books("CSS", "Ducket", "CSS", 300) },
-    { book3: new Books("JavaScript", "John Doe", "JavaScript", 500) },
-    { car1: new Cars("Vitz", "Honda", 2016, 1200, "black", 500000) },
-    { car2: new Cars("Corolla", "Toyota", 2016, 1500, "white", 540000) },
-    { car3: new Cars("CheryQQ", "Santro", 2016, 1000, "red", 205000) },
-    { mobile1: new Mobiles("S1", "Qmobile", "black", 5, 11000) },
-    { mobile2: new Mobiles("J1-Ace", "Samsung", "blue", 4.5, 19000) },
-    { mobile3: new Mobiles("X90", "Motorolla", "white", 5, 27000) }
+    { x: new Books("HTML & CSS", "Ducket", "HTML & CSS", 200) },
+    { x: new Books("Git Essentials", "Ducket", "Git", 700) },
+    { x: new Books("JavaScript", "John Doe", "JavaScript", 500) },
+    { x: new Cars("Vitz", "Honda", 2016, 1200, "black", 500000) },
+    { x: new Cars("Corolla", "Toyota", 2016, 1500, "white", 540000) },
+    { x: new Cars("CheryQQ", "Santro", 2016, 1000, "red", 205000) },
+    { x: new Mobiles("S1", "Qmobile", "black", 5, 11000) },
+    { x: new Mobiles("J1-Ace", "Samsung", "blue", 4.5, 19000) },
+    { x: new Mobiles("X90", "Motorolla", "white", 5, 27000) }
 ];
-// var b = ad[0].book1 instanceof Books;  //true
-// console.log(b);
-// console.log(ad[0]); // complete object {}
-// console.log(ad[0].book1);
-//console.log(ad[0].book1.display()); // the value returned by display
-//ad[0].book1.display();
+//console.log(ad[0]); // complete object {} jb k alert krane se it says object
+//console.log(ad[0].x);
+//alert(ad[0].x.display()); // the value returned by display
+// alert(ad[0] instanceof Books); //false
+//alert(ad[0].book instanceof Books); //true
+//alert(ad[4].x instanceof Books); //true for the later array
+//alert(typeof ad[0].x); // object     
+//alert(ad[0].x.cName); // yupieeeeee i got it !! :D className mil gya i.e. Books
 // display the All tab
-// let a: string;
-// for (var i = 0; i < ad.length; i++) {
-//     if(i<3) {
-//         a = ad[i].book1.display();
-//         document.getElementById("all").innerHTML += a;
-//     }
-//     else if(i<6 && i>2) {
-//         a = ad[i].car1.display();
-//         document.getElementById("all").innerHTML += a;
-//     }
-//     else {
-//         a = ad[i].mobile1.display();
-//         document.getElementById("all").innerHTML += a;
-//     }
-// }
-console.log(ad);
-//  document.getElementById("all").innerHTML += ;
+var a;
+for (var i = 0; i < ad.length; i++) {
+    var classDeterminator = ad[i].x.cName; // returns class name 
+    switch (classDeterminator) {
+        case 'Books': {
+            a = '<div class="panel panel-primary">' +
+                '<div class="panel-heading">' +
+                '<h3 class="panel-title" id="title">' +
+                ad[i].x.subject +
+                '</h3>' +
+                '</div>' +
+                '<div class="panel-body" id="content">' +
+                ad[i].x.display() +
+                '</div>' +
+                '</div>'; // creates new panel for book
+            document.getElementById("all").innerHTML += a;
+            break;
+        }
+        case 'Cars': {
+            a = '<div class="panel panel-primary">' +
+                '<div class="panel-heading">' +
+                '<h3 class="panel-title" id="title">' +
+                ad[i].x.name +
+                '</h3>' +
+                '</div>' +
+                '<div class="panel-body" id="content">' +
+                ad[i].x.display() +
+                '</div>' +
+                '</div>'; // creates new panel for car
+            document.getElementById("all").innerHTML += a;
+            break;
+        }
+        case 'Mobiles': {
+            a = '<div class="panel panel-primary">' +
+                '<div class="panel-heading">' +
+                '<h3 class="panel-title" id="title">' +
+                ad[i].x.company + ' ' + ad[i].x.model +
+                '</h3>' +
+                '</div>' +
+                '<div class="panel-body" id="content">' +
+                ad[i].x.display() +
+                '</div>' +
+                '</div>'; // creates new panel for mobile
+            document.getElementById("all").innerHTML += a;
+            break;
+        }
+        default: {
+            alert("Error!"); // yahan pohnchenge hi nhi hopefully
+        }
+    } // switch ends
+} // for-loop ends
 // display the Books tab
 var showBook;
 for (var i = 0; i <= 2; i++) {
-    showBook = ad[i].book1.display();
-    // showBook = "Title : " + ad[i].book1.title + 
-    //                             "<br /> Author : " + ad[i].book1.author + 
-    //                             "<br /> Subject : " + ad[i].book1.subject +
-    //                             "<br /> Price : " + ad[i].book1.price +
-    //                             "<br /><br />";
+    showBook = '<div class="panel panel-primary">' +
+        '<div class="panel-heading">' +
+        '<h3 class="panel-title" id="title">' +
+        ad[i].x.subject +
+        '</h3>' +
+        '</div>' +
+        '<div class="panel-body" id="content">' +
+        ad[i].x.display() +
+        '</div>' +
+        '</div>'; // creating new panel with title and content for book 
     document.getElementById("books").innerHTML += showBook;
 }
 // display the Cars tab
 var showCar;
 for (var i = 3; i <= 5; i++) {
-    //showCar = ad[i].car1.display();
-    showCar = "Name : " + ad[i].car1.name +
-        "<br /> Company : " + ad[i].car1.company +
-        "<br /> Model : " + ad[i].car1.model +
-        "<br /> Engine : " + ad[i].car1.engine +
-        "<br /> Color : " + ad[i].car1.color +
-        "<br /> Price : " + ad[i].car1.price +
-        "<br /><br />";
+    showCar = '<div class="panel panel-primary">' +
+        '<div class="panel-heading">' +
+        '<h3 class="panel-title" id="title">' +
+        ad[i].x.name +
+        '</h3>' +
+        '</div>' +
+        '<div class="panel-body" id="content">' +
+        ad[i].x.display() +
+        '</div>' +
+        '</div>'; // creating new panel with title and content for car
     document.getElementById("cars").innerHTML += showCar;
 }
 // display the Mobiles tab
 var showMobile;
 for (var i = 6; i <= 8; i++) {
-    showMobile = ad[i].mobile1.display();
-    // "Model : " + ad[i].mobile1.model + 
-    //                             "<br /> Company : " + ad[i].mobile1.company + 
-    //                             "<br /> Color : " + ad[i].mobile1.color +
-    //                             "<br /> Screen Size : " +ad[i].mobile1.screenSize +
-    //                             "<br /> Price : " + ad[i].mobile1.price +
-    //                             "<br /><br />";
+    showMobile = '<div class="panel panel-primary">' +
+        '<div class="panel-heading">' +
+        '<h3 class="panel-title" id="title">' +
+        ad[i].x.company + ' ' + ad[i].x.model +
+        '</h3>' +
+        '</div>' +
+        '<div class="panel-body" id="content">' +
+        ad[i].x.display() +
+        '</div>' +
+        '</div>'; // creating new panel with title and content for mobile
     document.getElementById("mobiles").innerHTML += showMobile;
 }
-// var show : string = ad[0].book1.display();
-// document.getElementById("books").innerHTML += show; 
-//# sourceMappingURL=ads.js.map
--- ads.ts
+++ ads.ts
@@ -1,9 +1,11 @@
-class Books {
+class Books { 
+    cName : string = "Books";
     title : string;
     author : string;
     subject : string;
     price : number;
     
+    // constructor automatically assigns the argument values to class attributes whenever a new instance is generated
     constructor(titleOfBook: string, authorOfBook: string, subjectOfBook: string, priceOfBook: number) {
         this.title = titleOfBook;
         this.author = authorOfBook;
@@ -11,12 +13,13 @@ class Books {
         this.price = priceOfBook;
     }
 
-    display(): string {
+    display(): string { //returns all attributes with html wrapping
         return ("Title: " + this.title + "<br /> Author: " + this.author + "<br /> Subject: " + this.subject + "<br /> Price: Rs. " + this.price + "<br /><br />");
     }
 }
 
 class Cars {
+    cName : string = "Cars";
     name : string;
     company : string;
     model : number;
@@ -33,12 +36,13 @@ class Cars {
         this.price = priceOfCar;
     }
 
-    display(): string {
+    display(): string { //returns all attributes with html wrapping
          return ("Name: " + this.name + "<br /> Company: " + this.company + "<br /> Model: " + this.model + "<br /> Engine: " + this.engine + "cc <br /> Color: " + this.color + "<br /> Price: Rs. " + this.price + "<br /><br />");
     }
 }
 
 class Mobiles {
+    cName : string = "Mobiles";
     model: string;
     company: string;
     color: string;
@@ -53,92 +57,142 @@ class Mobiles {
         this.price = priceOfMobile;
     }
 
-    display(): string {
+    display(): string { //returns all attributes with html wrapping
         return ("Model: " + this.model + "<br /> Company: " + this.company + "<br /> Color: " + this.color +  "<br /> Screen Size: " + this.screenSize + ' "<br /> Price: Rs. ' + this.price + "<br /><br />");
     }
 }
 
-// var book1 = new Books("HTML", "Ducket", "HTML", 1200);
+// var book1 = new Books("HTML", "Ducket", "HTML", 1200); // creating new instance (i.e.object) of a class
 
-var ad : any[] = [
-    {book1 : new Books("HTML", "Ducket", "HTML", 200)},
-    {book2 : new Books("CSS", "Ducket", "CSS", 300)},
-    {book3 : new Books("JavaScript", "John Doe", "JavaScript", 500)},
-    {car1 : new Cars("Vitz", "Honda", 2016, 1200, "black", 500000)},
-    {car2 : new Cars("Corolla", "Toyota", 2016, 1500, "white", 540000)},
-    {car3 : new Cars("CheryQQ", "Santro", 2016, 1000, "red", 205000)},
-    {mobile1 :new Mobiles("S1", "Qmobile", "black", 5, 11000)},
-    {mobile2 :new Mobiles("J1-Ace", "Samsung", "blue", 4.5, 19000)},
-    {mobile3 :new Mobiles("X90", "Motorolla", "white", 5, 27000)}
+var ad : any[] = [ // hard coded array for ad listings
+    {x : new Books("HTML & CSS", "Ducket", "HTML & CSS", 200)},
+    {x : new Books("Git Essentials", "Ducket", "Git", 700)},
+    {x : new Books("JavaScript", "John Doe", "JavaScript", 500)},
+    {x : new Cars("Vitz", "Honda", 2016, 1200, "black", 500000)},
+    {x : new Cars("Corolla", "Toyota", 2016, 1500, "white", 540000)},
+    {x : new Cars("CheryQQ", "Santro", 2016, 1000, "red", 205000)},
+    {x :new Mobiles("S1", "Qmobile", "black", 5, 11000)},
+    {x :new Mobiles("J1-Ace", "Samsung", "blue", 4.5, 19000)},
+    {x :new Mobiles("X90", "Motorolla", "white", 5, 27000)}
 ]; 
 
-// var b = ad[0].book1 instanceof Books;  //true
-// console.log(b);
-// console.log(ad[0]); // complete object {}
-// console.log(ad[0].book1);
-//console.log(ad[0].book1.display()); // the value returned by display
+//console.log(ad[0]); // complete object {} jb k alert krane se it says object
+//console.log(ad[0].x);
+//alert(ad[0].x.display()); // the value returned by display
 
-//ad[0].book1.display();
+// alert(ad[0] instanceof Books); //false
+//alert(ad[0].book instanceof Books); //true
+//alert(ad[4].x instanceof Books); //true for the later array
+//alert(typeof ad[0].x); // object     
+
+//alert(ad[0].x.cName); // yupieeeeee i got it !! :D className mil gya i.e. Books
 
 // display the All tab
-// let a: string;
-// for (var i = 0; i < ad.length; i++) {
-//     if(i<3) {
-//         a = ad[i].book1.display();
-//         document.getElementById("all").innerHTML += a;
-//     }
-//     else if(i<6 && i>2) {
-//         a = ad[i].car1.display();
-//         document.getElementById("all").innerHTML += a;
-//     }
-//     else {
-//         a = ad[i].mobile1.display();
-//         document.getElementById("all").innerHTML += a;
-//     }
-// }
-console.log(ad);
+let a: string;
 
-      //  document.getElementById("all").innerHTML += ;
+for (var i = 0; i < ad.length; i++) {
+    let classDeterminator : string = ad[i].x.cName; // returns class name 
 
+    switch(classDeterminator) { // use class name to check from which class the object belongs
+        case 'Books': {
+            a = '<div class="panel panel-primary">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title" id="title">' +
+                                ad[i].x.subject +
+                            '</h3>' +
+                        '</div>' +
+                        '<div class="panel-body" id="content">'+
+                            ad[i].x.display() +
+                        '</div>' +
+                    '</div>'; // creates new panel for book
+
+            document.getElementById("all").innerHTML += a;
+            break;
+        }
+        case 'Cars' : {
+            a = '<div class="panel panel-primary">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title" id="title">' +
+                                ad[i].x.name +
+                            '</h3>' +
+                        '</div>' +
+                        '<div class="panel-body" id="content">'+
+                            ad[i].x.display() +
+                        '</div>' +
+                    '</div>'; // creates new panel for car
+
+            document.getElementById("all").innerHTML += a;
+            break;
+        }
+        case 'Mobiles' : {
+             a = '<div class="panel panel-primary">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title" id="title">' +
+                                ad[i].x.company + ' ' + ad[i].x.model +
+                            '</h3>' +
+                        '</div>' +
+                        '<div class="panel-body" id="content">'+
+                            ad[i].x.display() +
+                        '</div>' +
+                    '</div>';  // creates new panel for mobile
+
+            document.getElementById("all").innerHTML += a;
+            break;
+        }
+        default: {
+            alert("Error!"); // yahan pohnchenge hi nhi hopefully
+        }
+    } // switch ends
+
+    } // for-loop ends
 
 // display the Books tab
 let showBook : string;
-for(var i = 0; i <= 2; i++) { //both vars are working
-    showBook = ad[i].book1.display();
-    // showBook = "Title : " + ad[i].book1.title + 
-    //                             "<br /> Author : " + ad[i].book1.author + 
-    //                             "<br /> Subject : " + ad[i].book1.subject +
-    //                             "<br /> Price : " + ad[i].book1.price +
-    //                             "<br /><br />";
-    document.getElementById("books").innerHTML += showBook;
+for(var i = 0; i <= 2; i++) { 
+   showBook = '<div class="panel panel-primary">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title" id="title">' +
+                                ad[i].x.subject +
+                            '</h3>' +
+                        '</div>' +
+                        '<div class="panel-body" id="content">'+
+                            ad[i].x.display() +
+                        '</div>' +
+                    '</div>'; // creating new panel with title and content for book 
+   
+   document.getElementById("books").innerHTML += showBook;
 }
 
 // display the Cars tab
 let showCar : string;
 for (var i = 3; i <= 5 ; i++) {
-    //showCar = ad[i].car1.display();
-    showCar = "Name : " + ad[i].car1.name + 
-                                "<br /> Company : " + ad[i].car1.company + 
-                                "<br /> Model : " + ad[i].car1.model +
-                                "<br /> Engine : " + ad[i].car1.engine +
-                                "<br /> Color : " + ad[i].car1.color +
-                                "<br /> Price : " + ad[i].car1.price +
-                                "<br /><br />";
+    showCar = '<div class="panel panel-primary">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title" id="title">' +
+                                ad[i].x.name +
+                            '</h3>' +
+                        '</div>' +
+                        '<div class="panel-body" id="content">'+
+                            ad[i].x.display() +
+                        '</div>' +
+                    '</div>'; // creating new panel with title and content for car
+
     document.getElementById("cars").innerHTML += showCar;
 }
 
 // display the Mobiles tab
 let showMobile : string;
 for (var i = 6; i <= 8; i++) {
-    showMobile = ad[i].mobile1.display();
-    // "Model : " + ad[i].mobile1.model + 
-    //                             "<br /> Company : " + ad[i].mobile1.company + 
-    //                             "<br /> Color : " + ad[i].mobile1.color +
-    //                             "<br /> Screen Size : " +ad[i].mobile1.screenSize +
-    //                             "<br /> Price : " + ad[i].mobile1.price +
-    //                             "<br /><br />";
+    showMobile ='<div class="panel panel-primary">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title" id="title">' +
+                                ad[i].x.company + ' ' + ad[i].x.model +
+                            '</h3>' +
+                        '</div>' +
+                        '<div class="panel-body" id="content">'+
+                            ad[i].x.display() +
+                        '</div>' +
+                    '</div>'; // creating new panel with title and content for mobile
+
     document.getElementById("mobiles").innerHTML += showMobile;
 }
-
-// var show : string = ad[0].book1.display();
-// document.getElementById("books").innerHTML += show;
